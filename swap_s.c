4c259a76d7dfad55a63dad4f007a4965a1e610d2
amaquena <alvin1king@gmail.com>
2019-08-22 17:10:35 +0200

    added swap sa sb and ss in swap_s.c

--- swap_s.c
+++ swap_s.c
@@ -6,31 +6,31 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 15:30:55 by amaquena          #+#    #+#             */
-/*   Updated: 2019/08/19 14:27:50 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/08/22 16:57:05 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap_sa(int *stack_a)
+void	swap_sa(t_stack **stack_a)
+{
+	int temp;
+
+	temp = (*stack_a)->elem;
+	(*stack_a)->elem = (*stack_a)->next->elem;
+	(*stack_a)->next->elem = temp;
+}
+
+void	swap_sb(t_stack **stack_b)
 {
 	int		temp;
 
-	temp = stack_a[0];
-	stack_a[0] = stack_a[1];
-	stack_a[0] = temp;
+	temp = (*stack_b)->elem;
+	(*stack_b)->elem = (*stack_b)->next->elem;
+	(*stack_b)->next->elem = temp;
 }
 
-void	swap_sb(int *stack_b)
-{
-	int		temp;
-
-	temp = stack_b[0];
-	stack_b[1] = stack_b[0];
-	stack_b[0] = temp;
-}
-
-void	swap_ss(int *stack_a, int *stack_b)
+void	swap_ss(t_stack **stack_a, t_stack **stack_b)
 {
 	swap_sa(stack_a);
 	swap_sb(stack_b);
