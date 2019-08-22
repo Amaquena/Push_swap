03056cf5ad2361ba996809942a7add971c8bda16
amaquena <alvin1king@gmail.com>
2019-08-22 17:08:38 +0200

    edited

--- main.c
+++ main.c
@@ -2,25 +2,22 @@
 
 int main(int ac,char **av)
 {
-//	char *str;
-	t_stack *stack;
-	int i;
+	t_stack *stack_a;
+	t_stack *stack_b;
 	int j;
 
 	if (ac > 1)
 	{
-		i = 0;
 		j = ac - 1;
-		while (i < ac && j > 0)
-		{
-			push(&stack, ft_atoi(av[j]));
-			i++;
-			j--;
-		}
-		disp_stack(&stack);
-		pop(&stack);
+		while (j > 0)
+			push(&stack_a, ft_atoi(av[j--]));
+		j = ac - 1;
+		while (j > 0)
+			push (&stack_b, ft_atoi(av[j--]));
+		swap_ss(&stack_a, &stack_b);
+		disp_stack(&stack_a);
 		ft_putchar('\n');
-		disp_stack(&stack);
+		disp_stack(&stack_b);
 	//	str = strmerge(av + 1, ac - 1);
 	//	stack_a *head;
 	//	head = new_list(3);
