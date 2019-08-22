d14a03c853c0fad7de257912212f379b01ce4ea1
amaquena <alvin1king@gmail.com>
2019-08-22 17:09:46 +0200

    added swap sa sb and ss

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: amaquena <amaquena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 16:15:52 by amaquena          #+#    #+#             */
-/*   Updated: 2019/08/20 13:43:06 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/08/22 16:58:03 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,11 @@ void		disp_stack(t_stack **stacka);
 **	swap actions
 */
 
+
+void		swap_sa(t_stack **stack_a);
+void		swap_sb(t_stack **stack_b);
+void		swap_ss(t_stack **stack_a, t_stack **stack_b);
 /*
-void		swap_sa(int *stack_a);
-void		swap_sb(int *stack_b);
-void		swap_ss(int *stack_a, int *stack_b);
 void		swap_ra(int *stack_a, int size);
 void		swap_rb(int *stack_b, int size);
 void		swap_rr(int *stack_a, int *stack_b);
