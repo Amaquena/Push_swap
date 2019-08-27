6d7544c7155d9906bd3e93f399d96ba5e5e4e8f3
amaquena <alvin1king@gmail.com>
2019-08-27 16:27:30 +0200

    test file with itoa & atoi

--- main.c
+++ main.c
@@ -1,41 +1,11 @@
 #include "push_swap.h"
+#include <stdio.h>
 
 int main()
 {
-	char *line;
+	char *num = "2147483648";
+	int i = -1;
 
-	while (get_next_line(0, &line))
-	{
-		ft_putstr(line);
-		ft_putchar('\n');
-	}
-
-	/*
-	t_stack *stack_a;
-	t_stack *stack_b = NULL;
-	int j;
-
-	if (ac > 1)
-	{
-		j = ac - 1;
-		while (j > 0)
-			push(&stack_a, ft_atoi(av[j--]));
-		j = ac - 1;
-		while (j > 0)
-			push (&stack_b, ft_atoi(av[j--]));
-		ft_putstr("stack a: ");
-		disp_stack(stack_a);
-		ft_putstr("\t\tstack b: ");
-		disp_stack(stack_b);
-		ft_putchar('\n');
-		push_b(&stack_a, &stack_b);
-		push_b(&stack_a, &stack_b);
-		ft_putstr("stack a: ");
-		disp_stack(stack_a);
-		ft_putstr("\t\tstack b: ");
-		disp_stack(stack_b);
-		ft_putchar('\n');
-	}
-		*/
+	printf("atoi: %s\n", ft_itoa(ft_atoi(num)));
 	return (0);
 }
